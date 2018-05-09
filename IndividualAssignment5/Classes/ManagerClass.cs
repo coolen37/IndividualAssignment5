395d7e7bdeb38c55e7adbd3545dbbebb677df9a0
coolen37 <38044047+coolen37@users.noreply.github.com>
2018-05-08 23:16:00 -0500

    Import all

---
+++ IndividualAssignment5/Classes/ManagerClass.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace IndividualAssignment5.Classes
+{
+    class ManagerClass
+    {
+        [PrimaryKey]
+            public string managerEmail { get; set; }
+            public string managerName { get; set; }
+    }
+}
